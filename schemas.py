61c38d37200376f717569a79fff7c2c6ccb51889
ddisanto-langops <1daniel.disanto@gmail.com>
2026-06-17 17:26:15 -0400

    Simplify schema

--- schemas.py
+++ schemas.py
@@ -61,7 +61,7 @@ class EditProductRequest(BaseModel):
     trello_product_code: ProductCodes | None = None
     trello_target_language: Languages | None = None
     trello_due_date: datetime | None = None
-    trello_date_published: datetime | None  = None
+    trello_date_published: datetime | None = None
     trello_media_groups: list[MediaGroups] | None = None
     trello_editor_url: HttpUrl | None = None
     trello_article_url: HttpUrl | None = None
@@ -78,7 +78,6 @@ class EditProductRequest(BaseModel):
     crowdin_url: HttpUrl | None = None
     # soft delete
     date_deleted: datetime | None = None
-
 
 
 # --------------
@@ -111,32 +110,8 @@ class PaginatedProductResponse(BaseModel):
     data: list[GetProductResponse]
 
 
-class EditProductResponse(BaseModel):
+class EditProductResponse(EditProductRequest):
     model_config = ConfigDict(from_attributes=True)
-    # trello
-    trello_title: str | None = None
-    trello_url: HttpUrl | None = None
-    trello_product_code: ProductCodes | None = None
-    trello_target_language: Languages | None = None
-    trello_due_date: datetime | None = None
-    trello_date_published: datetime | None = None
-    trello_media_groups: list[MediaGroups] | None = None
-    trello_editor_url: HttpUrl | None = None
-    trello_article_url: HttpUrl | None = None
-    trello_word_count: int | None = None
-    # youtube
-    youtube_id: str | None = None
-    youtube_localized_title: str | None = None
-    youtube_url: HttpUrl | None = None
-    youtube_duration_seconds: int | None = None
-    # crowdin
-    crowdin_id: str | None = None
-    crowdin_translation_progress: float | None = None
-    crowdin_approval_progress: float | None = None
-    crowdin_url: HttpUrl | None = None
-    # soft delete
-    date_deleted: datetime | None = None
-
 
 
 class DeleteResponse(BaseModel):
