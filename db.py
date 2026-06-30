c89d0804d507e4a22080619b889460f0d568405d
ddisanto-langops <1daniel.disanto@gmail.com>
2026-06-30 10:37:37 -0400

    remove local testing variables

--- db.py
+++ db.py
@@ -9,11 +9,7 @@ HOST = os.environ.get("DB_HOST")
 DB_NAME = os.environ.get("DB_NAME")
 PORT = os.environ.get("DB_PORT")
 
-# FOR LOCAL TESTING ------------------------------------------------------------------------------------
-HOST_DEV = os.getenv("DB_HOST_DEV")
-DATABASE_URL = os.getenv("URL", f"postgresql+asyncpg://{USER}:{PASSWORD}@{HOST_DEV}:{PORT}/{DB_NAME}")
-# ------------------------------------------------------------------------------------------------------
-#DATABASE_URL = f"postgresql+asyncpg://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
 
 engine = create_async_engine(
     DATABASE_URL,
