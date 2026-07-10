e8196d9a44b2e49ec1c69c9fb6348df6f49ce783
ddisanto-langops <1daniel.disanto@gmail.com>
2026-07-10 16:06:35 -0400

    Add check for len 1 audience tags

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ import os
 import jwt
 from fastapi import HTTPException, status, Request
 
-CF_TEAM_URL = os.getenv("CF_TEAM_URL").rstrip("/") + "/"
+CF_TEAM_URL = os.getenv("CF_TEAM_URL")
 TRUSTED_AUDIENCES = os.getenv("TRUSTED_AUDIENCES")
 CERTS_URL = f"{CF_TEAM_URL}/cdn-cgi/access/certs"
 
@@ -30,13 +30,15 @@ async def verify_jwt(request: Request) -> dict:
                 status.HTTP_500_INTERNAL_SERVER_ERROR, detail= "Target audience missing or not in listed of trusted audiences"
             )
         
+        target_audience = audiences[0] if len(audiences) == 1 else audiences
+        
         signing_key = jwks_client.get_signing_key_from_jwt(token)
 
         payload = jwt.decode(
             token,
             signing_key.key,
             algorithms=["RS256"],
-            audience=audiences,
+            audience=target_audience,
             issuer=CF_TEAM_URL,
             options={
                 "require": ["aud"]
